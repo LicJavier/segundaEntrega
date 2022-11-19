10a26d55a6d4434225272e06e8f5cd7a4252964a
Javier Alejandro Riquelme <lic.riquelme.j@gmail.com>
2022-11-19 14:37:54 -0300

    heroku app 1.0.5

--- src/utils/faker.js
+++ src/utils/faker.js
@@ -1,16 +1,16 @@
-import { faker } from '@faker-js/faker';
-faker.locale = 'es';
+// import { faker } from '@faker-js/faker';
+// faker.locale = 'es';
 
-export default function generarProducto() {
-    const producto = [];
-    for (let index = 0; index < 5; index++) {
-        producto.push(
-            {
-                "nombre": faker.commerce.product(),
-                "precio": faker.commerce.price(),
-                "image": faker.image.food( 200 , 200, true ),
-            }
-        )
-    }
-    return producto;
-}
+// export default function generarProducto() {
+//     const producto = [];
+//     for (let index = 0; index < 5; index++) {
+//         producto.push(
+//             {
+//                 "nombre": faker.commerce.product(),
+//                 "precio": faker.commerce.price(),
+//                 "image": faker.image.food( 200 , 200, true ),
+//             }
+//         )
+//     }
+//     return producto;
+// }
